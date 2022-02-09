d5bbed8e484f34fcc0aa15b3f61dd644e7f7105b
Dietmar Bätzel <dietmar.baetzel@bitpacker.de>
2022-02-09 06:58:04 +0100

    NKurs

--- Beispiele/index.php
+++ Beispiele/index.php
@@ -9,48 +9,6 @@
     <body>
         <h1>Beispiel index - Datei</h1>
 		
-		
-		<?php
-			echo 'ich bin php';
 			
-			$server 	= 'localhost';
-			$user 		= 'root';
-			$password 	= '';
-			$dbname		= 'world';
-			
-			/* Mit den Zugangsdaten die Verbindung zu Server und Datenbank herstellen */
-			$conn 		= mysqli_connect($server, $user, $password, $dbname);
-			
-			if(!$conn)		/* Die Verbindung konnte nicht hergestellt werden */
-				die('Das ging schief'); // Also alles abbrechen
-			
-			/* Formulierung einer Abfrage, die mehrere Datensätze liefert */ 
-			$sql 	= 'SELECT * FROM Country';
-			$result = mysqli_query($conn, $sql); // Resultat, aber noch keine Datensätze
-			if($result) {
-				echo '<pre>';
-				print_r($result);
-				echo '</pre>';
-				
-				/* 	durch mysqli_fetch_assoc wird immer der nächste Datensatz abgeholt 
-					und in der Variable $record gespeichert. 
-					die while Schleife überwacht dabei auch, ob $record einen Wert enthält => weiterer Schleifendurchlauf -
-					oder nicht => Schleife endet
-				*/
-				while($record = mysqli_fetch_assoc($result)) {
-					echo '<pre>';		// html pre-Tag ausgeben
-					print_r($record);	// Das assoziative Feld $record ausgeben
-					echo '</pre>';		// html pre-Tag wieder schließen
-				}
-			}
-			
-			
-		?>
-		
-		
-		
-		<?php
-			mysqli_close($conn);
-		?>
     </body>
 </html>
--- Beispiele/shop/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $servername     =   'localhost';
-    $user           =   'localemma';
-    $password       =   'tzY[5PfJb9I.!Ghq';
-    $dbname         =   'emmasshop';
-?>
