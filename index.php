50424cebf121e0994c6fb9a0867497dd42683786
Dietmar Bätzel <dietmar.baetzel@bitpacker.de>
2022-02-16 13:27:40 +0100

    16_02_2022

--- index.php
+++ index.php
@@ -7,11 +7,44 @@
 
 <body>
 	<h1>Datenbankverbindungen</h1>
-	
+	<!-- Ab hier sollen Newline Zeichen interpretiert werden -->
+	<pre>
 	<?php
+
+
 		
+		$server 	= 'localhost';
+		$user 		= 'root';
+		$password	= '';
+		$dbname		= 'world';
+
+			/* Kommentar
+			mehrzeilig 
+			*/
+			// Einzeilig 
+
+		$conn = mysqli_connect($server, $user, $password, $dbname);
+		
+		if(!$conn)
+			die('Keine Verbindung zur Datenbank');
+
+		echo 'Klappt';
+
+		$sql 	= 'SELECT * FROM country';
+		$result = mysqli_query($conn, $sql);
+
+		print_r($result);
+		if($result) {
+			$record = mysqli_fetch_assoc($result);
+			print_r($record);
+		}
+
 	?>
 
-	
+	</pre>
 </body>
 </html>
+
+<?php
+	mysqli_close($conn);
+?>
